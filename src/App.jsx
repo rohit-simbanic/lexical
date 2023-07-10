e0bd59460a5c792acd195837540fbba543fd6429
Rohit Mondal <rohit.simbanic2023@gmail.com>
2023-07-10 18:30:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./styles.css";
+import Editor from "./Editor";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
